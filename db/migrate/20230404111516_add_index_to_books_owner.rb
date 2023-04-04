82634a0bc3f8db889a64a4dc3dd2d2f4a080665e
ShravanEssence <shravankumar.essence@gmail.com>
2023-04-04 18:47:19 +0530

    Tried to add Index

--- db/migrate/20230404111516_add_index_to_books_owner.rb
+++ db/migrate/20230404111516_add_index_to_books_owner.rb
@@ -1,5 +1,5 @@
 class AddIndexToBooksOwner < ActiveRecord::Migration[7.0]
   def change
-    add_index :Books, :owner, name: "index_Books_on_owner"
+    add_index :Books, :owner
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_111516) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_105803) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_111516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "owner"
-    t.index ["owner"], name: "index_Books_on_owner"
   end
 
   create_table "comments", force: :cascade do |t|
